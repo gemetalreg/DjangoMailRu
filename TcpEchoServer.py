ff5cfe1fdbf346bfc79ca64959bf19e487f0d43a
itunereg <itunereg@gmail.com>
2017-09-23 14:50:56 +0300

    TCP echo server created

---
+++ TcpEchoServer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2
+
+import socket
+
+IP = "0.0.0.0"
+PORT = 2222
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((IP, PORT))
+sock.listen()
+
+while True:
+    conn, addr = sock.accept()
+
+    while True:
+        data = conn.recv(1024)
+
+        if data == "close":
+            break
+
+        conn.send(data)
