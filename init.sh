d6105676ecfee0bf4e166833cde4080fd272b6b1
itunereg <itunereg@gmail.com>
2017-09-25 12:21:45 +0300

    rm default

--- init.sh
+++ init.sh
@@ -6,9 +6,10 @@ mkdir -p /home/box/web/public/{img,css,js}
 touch /home/box/web/{public/empty.txt,uploads/empty.txt,etc/empty.txt}
 touch /home/box/web/public/{img/empty.txt,css/empty.txt,js/empty.txt}
 
-cp -pf /etc/nginx/nginx.conf /home/box/web/etc/nginx.conf
+#cp -f /etc/nginx/nginx.conf /home/box/web/etc/nginx.conf
 
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
