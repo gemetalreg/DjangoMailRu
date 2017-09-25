22a9a0a3ad90de0c02c79e00c5b8d6e646702d38
itunereg <itunereg@gmail.com>
2017-09-25 10:38:14 +0300

    add/update sh scripts

--- init.sh
+++ init.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+
+mkdir -p /home/box/web/{public, uploads, etc}
+mkdir -p /home/box/web/public/{img, css, js}
+
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+#sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-git init
-git remote add origin https://github.com/gemetalreg/DjangoMailRu.git
-git pull origin master
+#git init
+#git remote add origin https://github.com/gemetalreg/DjangoMailRu.git
+#git pull origin master
+
+#touch /home/box/web/empty.txt
+#git clone https://github.com/gemetalreg/DjangoMailRu.git /home/box/web
+#bash /home/box/web/init.sh
