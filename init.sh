9cad22edaa7d06ab7e594b50371f59f8df089341
gemetalreg <itunereg@gmail.com>
2017-09-25 21:04:24 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,7 +8,8 @@
 
 #cp -f /etc/nginx/nginx.conf /home/box/web/etc/nginx.conf
 
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 #sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
