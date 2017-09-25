777ea1a819774c755a67fce7400b969b690507ab
itunereg <itunereg@gmail.com>
2017-09-25 11:29:27 +0300

    fix makdir in init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir -p /home/box/web/{public, uploads, etc}
-mkdir -p /home/box/web/public/{img, css, js}
+mkdir -p /home/box/web/{public,uploads,etc}
+mkdir -p /home/box/web/public/{img,css,js}
 
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
