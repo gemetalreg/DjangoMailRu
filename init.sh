65ad291bbf3589dc77e05d034bc9152579fcde15
itunereg <itunereg@gmail.com>
2017-09-25 11:58:17 +0300

    create empty and copy config

--- init.sh
+++ init.sh
@@ -3,9 +3,14 @@
 mkdir -p /home/box/web/{public,uploads,etc}
 mkdir -p /home/box/web/public/{img,css,js}
 
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+empty = "empty.txt"
+touch /home/box/web/{public/$(empty),uploads/$(empty),etc/$(empty)}
+touch /home/box/web/public/{img/$(empty),css/$(empty),js/$(empty)}
+
+cp -pf /etc/nginx/nginx.conf /home/box/web/etc/nginx.conf
+
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
+#sudo /etc/init.d/mysql start
