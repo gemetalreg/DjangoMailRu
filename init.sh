008389e08ef366cb080dcf7b1aa1ddad73ca2c67
itunereg <itunereg@gmail.com>
2017-09-28 11:54:51 +0300

    update start.sh and init.sh for django

--- init.sh
+++ init.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-# mkdir -p /home/box/web/{public,uploads,etc}
-# mkdir -p /home/box/web/public/{img,css,js}
-
-# touch /home/box/web/{public/empty.txt,uploads/empty.txt,etc/empty.txt}
-# touch /home/box/web/public/{img/empty.txt,css/empty.txt,js/empty.txt}
-
-#cp -f /etc/nginx/nginx.conf /home/box/web/etc/nginx.conf
 
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
--- start.sh
+++ start.sh
@@ -5,5 +5,20 @@
 #git pull origin master
 
 #touch /home/box/web/empty.txt
+
+# mkdir -p /home/box/web/{public,uploads,etc}
+# mkdir -p /home/box/web/public/{img,css,js}
+
+# touch /home/box/web/{public/empty.txt,uploads/empty.txt,etc/empty.txt}
+# touch /home/box/web/public/{img/empty.txt,css/empty.txt,js/empty.txt}
+
+#cp -f /etc/nginx/nginx.conf /home/box/web/etc/nginx.conf
+
 #git clone https://github.com/gemetalreg/DjangoMailRu.git /home/box/web
+
+#cd /home/box/web
+#django-admin startproject ask
+#python manage.py startapp qa
+
 #bash /home/box/web/init.sh
+
