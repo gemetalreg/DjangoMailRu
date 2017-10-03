5f8416ec65d674fbb8b82d210cb7798d70357dff
itunereg <itunereg@gmail.com>
2017-10-03 10:56:55 +0300

    Qusetion manager model fix

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,6 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
 class Question(models.Model):
     objects = QuestionManager()
 
@@ -19,9 +27,3 @@ class Answer(models.Model):
     question = models.ForeignKey(Question)
     author = models.ForeignKey(User)
 
-class QuestionManager(models.Model):
-    def new(self):
-        return self.order_by('-added_at')
-
-    def popular(self):
-        return self.order_by('-rating')
