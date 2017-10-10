b81e3a69cbe1802eddfd21a30773a42aab32b6e1
itunereg <itunereg@gmail.com>
2017-10-10 16:44:27 +0300

    fix models bland and null

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -20,8 +20,8 @@ class Question(models.Model):
 
     title = models.CharField(max_length = 255)
     text = models.TextField()
-    added_at = models.DateField(auto_now_add=True)
-    rating = models.IntegerField(default=0)
+    added_at = models.DateField(blank=True, auto_now_add=True)
+    rating = models.IntegerField(blank=True, null=True)
     author = models.ForeignKey(User, on_delete = models.CASCADE)
     likes = models.ManyToManyField(User, related_name='question_like_user')
 
@@ -33,7 +33,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateField(auto_now_add = True)
+    added_at = models.DateField(blank=True, auto_now_add = True)
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
     author = models.ForeignKey(User, on_delete = models.CASCADE)
 
