09777b0ffed1181e8714607c15fca20c4bc84638
itunereg <itunereg@gmail.com>
2017-10-12 12:56:17 +0300

    fix self pk

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -29,7 +29,7 @@ class Question(models.Model):
         return self.title
 
     def get_url(self):
-        return "/question/%s/" % self.id
+        return "/question/%s/" % self.pk
 
 class Answer(models.Model):
     text = models.TextField()
