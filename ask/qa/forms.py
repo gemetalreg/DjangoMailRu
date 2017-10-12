94b199b648b76917ccb586e874f870a8e9662caa
itunereg <itunereg@gmail.com>
2017-10-12 13:29:31 +0300

    set pass cleans

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -12,19 +12,10 @@ class AskForm(forms.Form):
     def clean_title(self):
         data_title = self.cleaned_data['title']
 
-        clear_title = data_title.strip()
-        if clear_title == "":
-            raise forms.ValidationError(u"Title is empty")
-
         return data_title
 
     def clean_text(self):
         data_text = self.cleaned_data['text']
-
-        clear_text = data_text.strip()
-
-        if clear_text == "":
-            raise forms.ValidationError(u"Text is empty")
 
         return data_text
 
@@ -40,10 +31,6 @@ class AnswerForm(forms.Form):
 
     def clean_text(self):
         data_text = self.cleaned_data['text']
-
-        clear_text = data_text.strip()
-        if clear_text == "":
-            raise forms.ValidationError(u"Text answer is empty")
 
         return data_text
 
