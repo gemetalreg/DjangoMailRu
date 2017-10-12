9ca63ed908021d1d0bf26234e7ca9bb30abfbe15
itunereg <itunereg@gmail.com>
2017-10-12 13:11:10 +0300

    back and fix cleans

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -9,33 +9,25 @@ class AskForm(forms.Form):
     title = forms.CharField(max_length=255)
     text = forms.CharField(widget=forms.Textarea)
 
-    # def clean_title(self):
-    #     data_title = self.cleaned_data['title']
-    #
-    #     clear_title = data_title.strip()
-    #
-    #     for error in errors_list:
-    #         error_res, error_mes = error(clear_title, u"title")
-    #         if error_res:
-    #             raise forms.ValidationError(error_mes)
-    #
-    #     return data_title
-    #
-    # def clean_text(self):
-    #     data_text = self.cleaned_data['text']
-    #
-    #     clear_text = data_text.strip()
-    #     errors_list = [is_epmty]
-    #
-    #     for error in errors_list:
-    #         error_res, error_mes = error(clear_text, u"text")
-    #         if error_res:
-    #             raise forms.ValidationError(error_mes)
-    #
-    #     return data_text
+    def clean_title(self):
+        data_title = self.cleaned_data['title']
 
-    def clean(self):
-        pass
+        clear_title = data_title.strip()
+        if clear_title == "":
+            raise forms.ValidationError(u"Title is empty")
+
+        return data_title
+
+    def clean_text(self):
+        data_text = self.cleaned_data['text']
+
+        clear_text = data_text.strip()
+
+        if clear_text == "":
+            raise forms.ValidationError(u"Text is empty")
+
+        return data_text
+
 
     def save(self):
         question = Question(**self.cleaned_data)
@@ -46,32 +38,25 @@ class AnswerForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea)
     question = forms.IntegerField(forms.HiddenInput)
 
-    # def clean_text(self):
-    #     data_text = self.cleaned_data['text']
-    #
-    #     clear_text = data_text.strip()
-    #     errors_list = [is_epmty]
-    #
-    #     for error in errors_list:
-    #         error_res, error_mes = error(clear_text, u"text")
-    #         if error_res:
-    #             raise forms.ValidationError(error_mes)
-    #
-    #     return data_text
-    #
-    # def clean_question(self):
-    #
-    #     data_question = self.cleaned_data['question']
-    #
-    #     try:
-    #         int_question = int(data_question)
-    #     except ValueError:
-    #         raise forms.ValidationError(u"Invalid question")
-    #
-    #     return int_question
+    def clean_text(self):
+        data_text = self.cleaned_data['text']
 
-    def clean(self):
-        pass
+        clear_text = data_text.strip()
+        if clear_text == "":
+            raise forms.ValidationError(u"Text answer is empty")
+
+        return data_text
+
+    def clean_question(self):
+
+        data_question = self.cleaned_data['question']
+
+        try:
+            int_question = int(data_question)
+        except ValueError:
+            raise forms.ValidationError(u"Invalid question")
+
+        return int_question
 
     def save(self):
         self.cleaned_data['question'] = get_object_or_404(Question,
