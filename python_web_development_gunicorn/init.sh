90550761ee6b543538ada002479522696d692e13
gemetalreg <itunereg@gmail.com>
2020-10-24 21:12:18 +0300

    Add files via upload

---
+++ python_web_development_gunicorn/init.sh
@@ -0,0 +1,23 @@
+# install nginx
+# main: /etc/nginx/{nginx.conf|sites-available}
+sudo apt-get update
+sudo apt-get install nginx
+
+# create folders
+mkdir -p /home/box/web/public /home/box/web/uploads /home/box/web/etc
+mkdir -p /home/box/web/public/img /home/box/web/public/css /home/box/web/public/js
+
+# create weak link to sites-enabled
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+
+# delete default virtual host
+sudo rm -f /etc/nginx/sites-enabled/default
+
+# create weak link gunicorn
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+
+# run nginx
+sudo /etc/init.d/nginx restart
+
+# run gunicorn
+sudo gunicorn -b 0.0.0.0:8080 hello:app
